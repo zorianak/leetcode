2192ebf76430d9e8cffb158913f7f6621ff8f62e
Callie Holmes <github@kimiscool.me>
2025-02-23 12:41:23 -0800

    Set this up to be able to run it outside of jsfiddle, add in problem (but not complete) find-elements-contaminated-tree

---
+++ Medium/find-elements-contaminated-tree.ts
@@ -0,0 +1,31 @@
+// description: https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/?envType=daily-question&envId=2025-02-23
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class FindElements {
+    constructor(root: TreeNode | null) {
+        
+    }
+
+    find(target: number): boolean {
+        
+    }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * var obj = new FindElements(root)
+ * var param_1 = obj.find(target)
+ */
